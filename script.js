1ab60dae3edaea17cf68fffd7a7a97c02b054efb
Keonity <56704285+Keonity@users.noreply.github.com>
2024-05-21 00:54:46 -0700

    Created virtual etch and sketch per Odin Project
    Implemented basic functionality allowing users to draw with their mouse on a fine-tunable grid.

---
+++ script.js
@@ -0,0 +1,41 @@
+let body = document.querySelector("body");
+console.log(body);
+
+let divContainer = document.querySelector(".container");
+
+let createButton = document.querySelector("#create");
+
+createButton.addEventListener("click", function() {
+    let size = prompt("Enter a grid size(height & width");
+    createBoard(size);
+});
+
+function createDiv() {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("squares");
+    newDiv.addEventListener('mouseover', function() {
+        newDiv.style.backgroundColor = 'salmon';
+    })
+    return newDiv;
+}
+
+function createRow(amount) {
+    let divRow = document.createElement("div");
+    divRow.classList.add("row");
+    for (let i = 0; i < amount; i++) {
+        divRow.appendChild(createDiv());
+    }
+    // divContainer.appendChild(divRow);
+    return divRow;
+}
+
+function createBoard(amount = 16) {
+    while (amount > 100) {
+        amount = prompt("Size cannot be greater than 100");
+    }
+    console.log("Create Board");
+    divContainer.replaceChildren();
+    for (let i = 0; i < amount; i++) {
+        divContainer.appendChild(createRow(amount));
+    }
+}
